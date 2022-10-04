34529b4ca6cf79b5aa85bbda91677a1139a58b8e
Jongryeol Han <orb.cov@gmail.com>
2022-10-04 01:31:33 +0000

    Add basic operations of truth value

--- src/truthvalue/truthvalue.cpp
+++ src/truthvalue/truthvalue.cpp
@@ -6,6 +6,11 @@ namespace omm {
         const string TruthValue::stringValues[3] = { "false", "true", "unknown" }; // for toString
         const TruthValue TruthValue::truthValues[3] = { TruthValue(0), TruthValue(1), TruthValue(2) }; // for singleton
 
+        const int TruthValue::andValues[3][3] = { {0, 0, 0}, {0, 1, 2}, {0, 2, 2} }; // and
+        const int TruthValue::orValues[3][3] = { {0, 1, 2}, {1, 1, 1}, {2, 1, 2} }; // or
+        const int TruthValue::xorValues[3][3] = { {0, 1, 2}, {1, 0, 2}, {2, 2, 2} }; // xor
+        const int TruthValue::notValues[3] = { 1, 0, 2 }; // not
+
         // constructor
         TruthValue::TruthValue(int value) : value(value) {}
 
@@ -28,5 +33,11 @@ namespace omm {
         int TruthValue::getValue() const { return this->value; }
         string TruthValue::toString() const { return TruthValue::stringValues[this->value]; }
         std::ostream& operator<<(std::ostream& os, const TruthValue& value) { return os << value.toString(); }
+
+        // operations
+        TruthValue TruthValue::operator&(const TruthValue& other) const { return TruthValue::truthValues[TruthValue::andValues[this->value][other.value]]; }
+        TruthValue TruthValue::operator|(const TruthValue& other) const { return TruthValue::truthValues[TruthValue::orValues[this->value][other.value]]; }
+        TruthValue TruthValue::operator^(const TruthValue& other) const { return TruthValue::truthValues[TruthValue::xorValues[this->value][other.value]]; }
+        TruthValue TruthValue::operator!() const { return TruthValue::truthValues[TruthValue::notValues[this->value]]; }
     }
 }
--- src/truthvalue/truthvalue.h
+++ src/truthvalue/truthvalue.h
@@ -14,6 +14,12 @@ namespace omm {
             static const string stringValues[3]; // for toString
             static const TruthValue truthValues[3]; // for singleton
 
+            // operator table
+            static const int andValues[3][3]; // and
+            static const int orValues[3][3]; // or
+            static const int xorValues[3][3]; // xor
+            static const int notValues[3]; // not
+
             // ctor
             TruthValue(int value);
         public:
@@ -26,6 +32,12 @@ namespace omm {
             int getValue() const;
             string toString() const;
             friend std::ostream& operator<<(std::ostream& os, const TruthValue& value);
+
+            // operations
+            TruthValue operator&(const TruthValue& other) const;
+            TruthValue operator|(const TruthValue& other) const;
+            TruthValue operator^(const TruthValue& other) const;
+            TruthValue operator!() const;
         };
     }
 }
