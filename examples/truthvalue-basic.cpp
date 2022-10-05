01849d4973d565a550a2479e80f0110ddf52c7b4
Jongryeol Han <orb.cov@gmail.com>
2022-10-05 00:06:54 +0000

    Add example code of basic operations

--- examples/truthvalue-basic.cpp
+++ examples/truthvalue-basic.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "src/omm.h"
+#include "src/truthvalue/truthvalue.h"
 
 using namespace std;
 using namespace omm::truthvalue;
@@ -10,4 +10,19 @@ int main() {
 
     cout << a << " " << b << " " << c << endl;
     cout << d.getValue() << " " << e.getValue() << " " << f.getValue() << endl;
+    cout << endl;
+
+    cout << "FIRST\tSECOND\t\t&  and\t|  or\t^  xor" << endl<< "--------------------------------------------" << endl;
+
+    TruthValue vals[3] = {a, b, c};
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << vals[i] << "\t" << vals[j] << "\t\t";
+            cout << (vals[i] & vals[j]) << "\t" << (vals[i] | vals[j]) << "\t" << (vals[i] ^ vals[j]) << endl;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        cout << "!" << vals[i] << " = " << !vals[i] << endl;
+    }
 }
